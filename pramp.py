630c7f89a0f084aa16b3a4ecac6f8b9d47ed776a
ailurophile <lisalitchfield@aim.com>
2017-07-28 17:31:34 -0700

    Return most frequent words first in word_count_engine().

--- pramp.py
+++ pramp.py
@@ -35,22 +35,17 @@ def counting_sort(tuples):
     counts = [0 for _ in range(max+1)]
     for word, count in tuples:
         counts[count] += 1  #counts array now holds number of words of frequency index
-    print counts
     sum = 0
     for i in range(len(counts)):
         sum += counts[i]
         counts[i] = sum  #counts array now holds number of word frequencies equal to or smaller than index
 #dimension output array
-    print counts
     output = [("dummy",0) for _ in range(len(tuples)+1)]
-    print "output = %s" % output
     for tuple in tuples:
 
         output[counts[tuple[1]]] = tuple
         counts[tuple[1]] -= 1
-        print output
-        print counts
-    return output[1:]
+    return output[-1:1:-1]
 
 
 if __name__ == '__main__':
