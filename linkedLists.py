4fbe30cd52abbd9299d843c7bd6517ddc1271e08
ailurophile <lisalitchfield@aim.com>
2017-08-18 14:56:42 -0700

    Implement function to remove duplicate values from a linked list.

--- linkedLists.py
+++ linkedLists.py
@@ -3,8 +3,8 @@ class Node(object):
     def __init__(self, data, previous=None, nextNode=None):
         self.data = data
         self.previous = previous
-        self.nextNode = None
-        
+        self.nextNode = nextNode
+
 class DoubleLinkedList(object):
     def __init__(self, head=None, tail = None):
         self.head = head
@@ -12,7 +12,7 @@ class DoubleLinkedList(object):
             self.tail = self.head
         else:
             self.tail = tail
-        
+
     def __str__(self):
         out = []
         if self.head == None: return''
@@ -22,7 +22,7 @@ class DoubleLinkedList(object):
             ptr = ptr.nextNode
         out.append(ptr.data)
         return str(out)
-    
+
     def addData(self,data):
         node = Node(data,self.tail)
         if self.head == None:
@@ -30,9 +30,9 @@ class DoubleLinkedList(object):
         if self.tail != None:
             self.tail.nextNode = node
         self.tail = node
-        
+
     def sumLists(self,List):
-        if List  == None: 
+        if List  == None:
             return None
         carry = 0
         sumList = DoubleLinkedList()
@@ -62,7 +62,15 @@ class DoubleLinkedList(object):
                 sumList.addData(carry)
                 carry = 0
         return sumList
-    
+def remove_dups(l):
+    current = l.head
+    values = dict()
+    while current != None:
+        if current.data not in values:
+            values[current.data] = 1
+        else:
+            current.previous.nextNode = current.nextNode
+        current = current.nextNode
 def isPalindrome(head,tail):
     if head == tail:
         return True   #single element palindrome
@@ -71,8 +79,8 @@ def isPalindrome(head,tail):
     if head.nextNode == tail:
         return True   #two element list both equal
     return isPalindrome(head.nextNode,tail.previous)
-    
-    
+
+
 if __name__ == '__main__':
     number1 = DoubleLinkedList(Node(7))
     number1.addData(1)
@@ -84,7 +92,9 @@ if __name__ == '__main__':
     number2.addData(9)
     print number2
     print number1.sumLists(number2)
-    tests = [[3],[1,1],[1,2],[1,2,3,3,2,1]]
+    remove_dups(number1)
+    print number1
+    tests = [[3],[1,1],[1,2],[1,2,3,3,2,1],[1,2,3,1,4,5,2,6]]
     def makelists(inputs):
         linkedLists = []
         for each in inputs:
@@ -97,3 +107,5 @@ if __name__ == '__main__':
     for l in lists:
         print l
         print isPalindrome(l.head,l.tail)
+    remove_dups(lists[-1])
+    print lists[-1]
